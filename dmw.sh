2932ef6230dc60f311b533d5d013f35831bd5571
JsonPager <144886165+JsonPager@users.noreply.github.com>
2024-08-26 14:10:20 +0800

    Update dmw.sh

--- dmw.sh
+++ dmw.sh
@@ -1,13 +1,10 @@
-# 获取当前主机名
-hostname=$(hostname)
-
 # 检查 Docker 服务状态
 check_docker_status() {
-  ssh root@${hostname} "systemctl status docker | grep 'running'" > /dev/null 2>&1
+  systemctl status docker | grep 'running' > /dev/null 2>&1
   if [ $? -eq 0 ]; then
-    echo "Docker 服务在 $hostname 上正在运行"
+    echo "Docker 服务正在运行"
   else
-    echo "Docker 服务在 $hostname 上未运行"
+    echo "Docker 服务未运行"
   fi
 }
 
