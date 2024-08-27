f4aa6a885ff2430e5aec40b9553b61dc7cf1a18a
JsonPager <144886165+JsonPager@users.noreply.github.com>
2024-08-27 10:36:36 +0800

    Update undocker.sh

--- undocker.sh
+++ undocker.sh
@@ -1,1 +1,18 @@
+#!/bin/bash
+
+# 检查docker服务是否存在
+if ! command -v docker &>/dev/null; then
+    echo "Docker服务不存在"
+    exit 1
+fi
+
+# 检查 Docker 服务是否正在运行
+if ! systemctl is-active docker; then
+    echo "Docker服务未运行，正在启动..."
+    systemctl start docker
+    if ! systemctl is-active docker; then
+        echo "启动 Docker 服务失败，正在卸载 Docker..."
+        exit 1
+    fi
+fi
 
