cf186edaa1b74b22ae6f3f0bd115475992358810
JsonPager <144886165+JsonPager@users.noreply.github.com>
2024-08-27 10:41:46 +0800

    Update mysh.sh

--- mysh.sh
+++ mysh.sh
@@ -4,16 +4,18 @@ cd /home
 echo "请选择要执行的操作："
 echo "1. 自动安装Docker+MPN+MySql+Wordpress"
 echo "2. 卸载Docker"
+echo "3. 退出"
 
 while true; do
     read -p "请输入您的选择: " choice
     case $choice in
     1)
-        echo "您选择了方法一"
+        echo "自动安装Docker+MPN+MySql+Wordpress"
         # ls
         ;;
     2)
-        echo "您选择了方法二"
+        echo "卸载Docker"
+        curl -L https://raw.githubusercontent.com/JsonPager/docker/main/undocker.sh -o undocker.sh && chmod +x undocker.sh && ./undocker.sh
         ;;
     3)
         echo "退出"
