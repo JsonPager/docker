ecf610efd84d9bcd8949fa4f1a3bb10c0537ea00
JsonPager <144886165+JsonPager@users.noreply.github.com>
2024-08-28 11:10:10 +0800

    Update dmw.sh

--- dmw.sh
+++ dmw.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 # 安装docker
 install_docker() {
     #查看架构
@@ -52,220 +50,31 @@ install_docker() {
     fi
 }
 
-# 检测mynet网络是否存在
-function check_network() {
-    network_exists=$(docker network ls -q --filter name=mynet 2>/dev/null)
-
-    if [[ -z "$network_exists" ]]; then
-        echo 1
+# 检查 Docker 服务状态
+startbuild() {
+    systemctl status docker | grep 'running' >/dev/null 2>&1
+    if [ $? -eq 0 ]; then
+        echo "Docker服务正在运行，执行后续操作"
+        echo "创建网络mynet"
+        docker network create --driver bridge --subnet 192.168.0.0/24 --gateway 192.168.0.1 mynet
+        echo "创建npm容器"
+        docker run --privileged=true -itd --restart=always --name=npm -p 80:80 -p 81:81 -p 443:443 --network=mynet --ip 192.168.0.2 -v /opt/dockerservice/npm/data:/data -v /opt/dockerservice/npm/letsencrypt:/etc/letsencrypt jc21/nginx-proxy-manager:latest
+        echo "创建mysql容器"
+        docker run --privileged=true -itd --restart=always --name mysql -p 33306:3306 --network=mynet --ip 192.168.0.3 -v /opt/dockerservice/mysql:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=lanlongning -e MYSQL_DATABASE=wordpressdb -e MYSQL_AUTHENTICATION_PLUGIN=mysql_native_password mysql:latest
+        echo "创建wordpress容器"
+        docker run --privileged=true -itd --restart=always --name=wordpress -p 8000:80 --network=mynet --ip 192.168.0.4 -v /opt/dockerservice/wordpress:/var/www/html -e WORDPRESS_DB_HOST=192.168.0.3:3306 -e WORDPRESS_DB_USER=root -e WORDPRESS_DB_PASSWORD=lanlongning -e WORDPRESS_DB_NAME=wordpressdb wordpress
     else
-        echo 0
+        echo "Docker服务未运行"
     fi
 }
 
-# 定义检查容器是否存在的方法
-function check_container_exists() {
-    container_name="$1" # 获取传入的容器名称
-
-    # 检查容器是否存在，并返回结果
-    if docker inspect -f '{{.Name}}' "$container_name" >/dev/null 2>&1; then
-        # 容器存在，返回0
-        return 0
-    else
-        # 容器不存在，返回1
-        return 1
-    fi
-}
-
-# 生成一个新的mynetip地址
-function getmynetnewip() {
-
-    # 获取网络名称
-    local network_name="mynet"
-
-    # 获取所有容器的 IPv4 地址，并去除子网掩码
-    local ips=($(docker network inspect "$network_name" | jq -r '.[0].Containers[].IPv4Address' | cut -d/ -f1))
-
-    # 打印数组中的每个 IP 地址
-    #for ip in "${ips[@]}"; do
-    #    echo "$ip"
-    #done
-
-    # 按字典序排序，最大的 IP 会排在最后
-    local sorted_ips=($(echo "${ips[@]}" | sort -V))
-
-    # 取出最后一个元素，即最大的 IP
-    local max_ip="${sorted_ips[-1]}"
-
-    #echo "最大的 IP 地址是：$max_ip"
-
-    IFS='.' read -ra ip_parts <<<"$max_ip"
-
-    # 对最低位加 1，并处理进位
-    ip_parts[3]=$((ip_parts[3] + $1))
-    for ((i = 3; i >= 1; i--)); do
-        if [ ${ip_parts[$i]} -gt 255 ]; then
-            ((ip_parts[$i] %= 256))
-            ((ip_parts[$i - 1]++))
-        fi
-    done
-
-    # 将四段重新组合成 IP 地址
-    # new_ip="${ip_parts[*]}"
-    local new_ip=$(
-        IFS='.'
-        echo "${ip_parts[*]}"
-    )
-
-    echo "$new_ip"
-}
-
-# 生成一个本地未被使用的随机端口号
-function get_unused_port() {
-    local start_port=1024
-    local end_port=65535
-    local port
-
-    # 随机生成一个端口号
-    port=$(shuf -i "$start_port-$end_port" -n 1)
-
-    # 检查端口是否被占用，并重试直到找到空闲端口
-    while true; do
-        if ! netstat -tulnp | grep -q ":${port}/"; then
-            break # 端口未被占用，退出循环
-        fi
-        # 端口被占用，重新生成随机端口
-        port=$(shuf -i "$start_port-$end_port" -n 1)
-    done
-
-    echo "$port"
-}
-
-# 检查docker服务是否存在，确定docker服务运行
+# 检查docker服务是否存在
 if command -v docker &>/dev/null; then
     echo "Docker服务已存在"
-    # 检查 Docker 服务是否正在运行
-    if ! systemctl is-active docker; then
-        echo "Docker服务未运行，正在启动..."
-        systemctl start docker
-        if ! systemctl is-active docker; then
-            echo "启动 Docker 服务失败，退出安装..."
-            exit 1
-        else
-            echo "Docker 服务启动成功！"
-        fi
-    else
-        echo "Docker 运行中"
-    fi
+    exit 1
 else
-    echo "未检测到Docker服务，开始安装"
     # 安装docker
     install_docker
-
-    if ! systemctl is-active docker; then
-        echo "启动 Docker 服务失败，退出安装..."
-        exit 1
-    fi
+    # 开始构建容器
+    startbuild
 fi
-
-sleep 5
-
-# 检测网络是否存在，不存在就创建网络
-checknetresult=$(check_network)
-if [[ $checknetresult -eq 0 ]]; then
-    echo "已存在mynet网络"
-else
-    echo "创建mynet网络"
-    docker network create --driver bridge --subnet 192.168.0.0/24 --gateway 192.168.0.1 mynet
-fi
-
-# 检查npm是否存在，不存在就创建npm
-if check_container_exists "npm"; then
-    echo "容器npm已存在"
-else
-    echo "容器npm不存在,创建npm容器,默认ip(192.168.0.2)，默认端口(81)"
-    docker run --privileged=true -itd --restart=always --name=npm -p 80:80 -p 81:81 -p 443:443 --network=mynet --ip 192.168.0.2 -v /opt/dockerservice/npm/data:/data -v /opt/dockerservice/npm/letsencrypt:/etc/letsencrypt jc21/nginx-proxy-manager:latest
-fi
-
-# 设置mysql容器名称
-while true; do
-    read -p "请输入mysql容器名称:" container_mysql
-    if [ -z "$container_mysql" ]; then
-        echo "请输入容器名称"
-        continue
-    fi
-    if check_container_exists "$container_mysql"; then
-        echo "容器 $container_mysql 存在"
-        continue
-    fi
-    break
-done
-
-# 设置mysql数据库名称
-while true; do
-    read -p "请输入mysql数据库名称:" mysql_dbname
-    if [ -z "$mysql_dbname" ]; then
-        echo "请输入数据库名称"
-        continue
-    fi
-    if [ ${#mysql_dbname} -lt 9 ]; then
-        echo "长度需要大于等于9"
-        continue
-    fi
-    break
-done
-
-# 设置mysql数据库密码
-while true; do
-    read -p "请输入mysql数据库密码:" mysql_passwd
-    if [ -z "$mysql_passwd" ]; then
-        echo "请输入数据库密码"
-        continue
-    fi
-    if [ ${#mysql_passwd} -lt 9 ]; then
-        echo "长度需要大于等于9"
-        continue
-    fi
-    break
-done
-
-# 设置mysql内网ip
-mysqlip=$(getmynetnewip 1)
-# 设置mysql外网映射端口号
-mysqlport=$(get_unused_port)
-
-echo "创建mysql容器"
-runmysql="docker run --privileged=true -itd --restart=always --name $container_mysql -p $mysqlport:3306 --network=mynet --ip $mysqlip -v /opt/dockerservice/$container_mysql:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=$mysql_passwd -e MYSQL_ROOT_HOST=% -e MYSQL_DATABASE=$mysql_dbname -e MYSQL_AUTHENTICATION_PLUGIN=mysql_native_password mysql:latest"
-eval $runmysql
-echo "创建mysql容器完成,重新启动容器"
-
-# 设置wordpress容器名称
-while true; do
-    read -p "请输入wordpress容器名称:" container_wordpress
-    if [ -z "$container_wordpress" ]; then
-        echo "请输入容器名称"
-        continue
-    fi
-    if check_container_exists "$container_wordpress"; then
-        echo "容器 $container_wordpress 存在"
-        continue
-    fi
-    break
-done
-
-# 设置wordpress内网ip
-wordpressip=$(getmynetnewip 2)
-# 设置wordpress外网映射端口号
-wordpressport=$(get_unused_port)
-
-echo "创建wordpress容器"
-runwordpress="docker run --privileged=true -itd --restart=always --name=$container_wordpress -p $wordpressport:80 --network=mynet --ip $wordpressip -v /opt/dockerservice/$container_wordpress:/var/www/html -e WORDPRESS_DB_HOST=$mysqlip:3306 -e WORDPRESS_DB_USER=root -e WORDPRESS_DB_PASSWORD=$mysql_passwd -e WORDPRESS_DB_NAME=$mysql_dbname wordpress"
-eval $runwordpress
-echo "创建wordpress容器完成"
-
-echo "mysql数据库:$mysql_dbname ,用户名:root, 密码: $mysql_passwd ,内网ip:$mysqlip ,外网映射端口: $mysqlport"
-echo "wordpress内网ip:$wordpressip ,外网映射端口:$wordpressport"
-
-# 开始构建容器
-# startbuild
-
