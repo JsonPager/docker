7e5d69a534b892845b208b7bb0cd8e49609ed3c3
JsonPager <144886165+JsonPager@users.noreply.github.com>
2024-08-28 11:20:33 +0800

    Update dmw_h.sh

--- dmw_h.sh
+++ dmw_h.sh
@@ -235,8 +235,8 @@ mysqlip=$(getmynetnewip 1)
 mysqlport=$(get_unused_port)
 
 echo "创建mysql容器"
-runmysql="docker run --privileged=true -itd --restart=always --name $container_mysql -p $mysqlport:3306 --network=mynet --ip $mysqlip -v /opt/dockerservice/$container_mysql:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=$mysql_passwd -e MYSQL_ROOT_HOST=% -e MYSQL_DATABASE=$mysql_dbname -e MYSQL_AUTHENTICATION_PLUGIN=mysql_native_password mysql:latest"
-eval $runmysql
+docker run --privileged=true -itd --restart=always --name "$container_mysql" -p "$mysqlport":3306 --network=mynet --ip "$mysqlip" -v /opt/dockerservice/"$container_mysql":/var/lib/mysql -e MYSQL_ROOT_PASSWORD="$mysql_passwd" -e MYSQL_ROOT_HOST=% -e MYSQL_DATABASE="$mysql_dbname" -e MYSQL_AUTHENTICATION_PLUGIN=mysql_native_password mysql
+
 echo "创建mysql容器完成,重新启动容器"
 
 # 设置wordpress容器名称
@@ -259,8 +259,8 @@ wordpressip=$(getmynetnewip 2)
 wordpressport=$(get_unused_port)
 
 echo "创建wordpress容器"
-runwordpress="docker run --privileged=true -itd --restart=always --name=$container_wordpress -p $wordpressport:80 --network=mynet --ip $wordpressip -v /opt/dockerservice/$container_wordpress:/var/www/html -e WORDPRESS_DB_HOST=$mysqlip:3306 -e WORDPRESS_DB_USER=root -e WORDPRESS_DB_PASSWORD=$mysql_passwd -e WORDPRESS_DB_NAME=$mysql_dbname wordpress"
-eval $runwordpress
+docker run --privileged=true -itd --restart=always --name="$container_wordpress" -p "$wordpressport":80 --network=mynet --ip "$wordpressip" -v /opt/dockerservice/"$container_wordpress":/var/www/html -e WORDPRESS_DB_HOST="$mysqlip":3306 -e WORDPRESS_DB_USER=root -e WORDPRESS_DB_PASSWORD="$mysql_passwd" -e WORDPRESS_DB_NAME="$mysql_dbname" wordpress
+
 echo "创建wordpress容器完成"
 
 echo "mysql数据库:$mysql_dbname ,用户名:root, 密码: $mysql_passwd ,内网ip:$mysqlip ,外网映射端口: $mysqlport"
